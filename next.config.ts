cf8c29449a74710bf69e448f666f06cc3403d138
Chalinee <chalinee.l@arise.tech>
2026-07-14 16:18:36 +0700

    feat: update hostname

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "nekfqpfhxaegaqlwasre.supabase.co",
+        hostname: "*.supabase.co",
       },
     ],
   },
